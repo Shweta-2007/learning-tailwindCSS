ef96e8fb91f2c5e96a89858b9d799f45c6e915a8
Shweta-2007 <sinhashweta713@gmail.com>
2024-01-04 21:23:16 +0530

    config && basic tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,240 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-gray-600 font-body grid md:grid-cols-3">
+      {/* Navbar */}
+      <div className="md:col-span-1 md:flex md:justify-end">
+        <nav className="text-right">
+          <div className="flex justify-between items-center">
+            <h1 className="font-bold uppercase p-4 border-b border-gray-100">
+              <a href="/" className="hover:text-gray-700">
+                Food Ninja
+              </a>
+            </h1>
+            <div className="px-4 cursor-pointer md:hidden" id="burger">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="w-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                />
+              </svg>
+            </div>
+          </div>
+          <ul className="text-sm mt-6 hidden md:block " id="menu">
+            <li className="text-gray-700 ">
+              <a
+                href="#"
+                className="px-4 flex justify-start mb-3 border-r-4 border-primary"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="w-5 mr-2"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                  />
+                </svg>
+
+                <span className="font-bold">Home</span>
+              </a>
+            </li>
+            <li>
+              <a href="#" className="px-4 flex justify-start mb-3">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="w-5 mr-2"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z"
+                  />
+                </svg>
+
+                <span>About</span>
+              </a>
+            </li>
+            <li>
+              <a href="#" className="px-4 flex justify-start">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="w-5 mr-2"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
+                  />
+                </svg>
+
+                <span>Contact</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      {/* main body */}
+      <main className="px-16 py-6 bg-gray-100 md:col-span-2">
+        <div className="flex justify-center md:justify-end">
+          <a
+            href="#"
+            className="text-primary btn border-primary md:border-2 hover:bg-primary hover:text-white transition ease-out duration-500"
+          >
+            Log in
+          </a>
+          <a
+            href="#"
+            className="text-primary ml-2 btn border-primary md:border-2 hover:bg-primary hover:text-white transition ease-out duration-500"
+          >
+            Sign up
+          </a>
+        </div>
+
+        <header>
+          <h2 className="text-gray-700 text-6xl font-semibold">Recipes</h2>
+          <h3 className="text-2xl font-semibold">For Ninjas</h3>
+        </header>
+
+        <div>
+          <h4 className="font-bold mt-12 pb-2 border-b border-gray-200">
+            Latest Recipes
+          </h4>
+
+          <div className="mt-8 grid lg:grid-cols-3 gap-10">
+            {/* Card-1 */}
+            <div className="card hover:shadow-lg">
+              <img
+                src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                className="w-full h-32 sm:h-48 object-cover"
+              />
+              <div className="m-4">
+                <span className="font-bold">5 Bean Chilli Stew</span>
+                <span className="block text-gray-500 text-sm">
+                  Recipe by Mario
+                </span>
+              </div>
+              <div className="badge">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="w-5 inline-block"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                  />
+                </svg>
+
+                <span>25 mins</span>
+              </div>
+            </div>
+            {/* Card-2 */}
+            <div className="card hover:shadow-lg">
+              <img
+                src="https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                className="w-full h-32 sm:h-48 object-cover"
+              />
+              <div className="m-4">
+                <span className="font-bold">Cooked food on black bowl</span>
+                <span className="block text-gray-500 text-sm">
+                  Recipe by Mario
+                </span>
+              </div>
+              <div className="badge">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="w-5 inline-block"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                  />
+                </svg>
+
+                <span>25 mins</span>
+              </div>
+            </div>
+            {/* Card-3 */}
+
+            <div className="card hover:shadow-lg">
+              <img
+                src="https://images.unsplash.com/photo-1484980972926-edee96e0960d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzF8fGZvb2R8ZW58MHx8MHx8fDA%3D"
+                className="w-full h-32 sm:h-48 object-cover"
+              />
+              <div className="m-4">
+                <span className="font-bold">Salad with guacamole</span>
+                <span className="block text-gray-500 text-sm">
+                  Recipe by Mario
+                </span>
+              </div>
+              <div className="badge">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="w-5 inline-block"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                  />
+                </svg>
+
+                <span>25 mins</span>
+              </div>
+            </div>
+          </div>
+
+          <h4 className="font-bold mt-12 pb-2 border-b border-gray-200">
+            Most Popular
+          </h4>
+
+          <div className="mt-8"></div>
+
+          <div className="flex justify-center">
+            <div className="bg-secondary-100 text-secondary-200 btn hover:shadow-inner transform hover:scale-125 hover:bg-opacity-50 transition ease-out duration-300">
+              Load more
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
